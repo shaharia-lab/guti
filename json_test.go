d977b2d27f3396442b97118d6d06a8a4daa6ba88
Shaharia Azam <mail@shaharia.com>
2023-04-08 14:52:54 +0200

    Fix

--- json_test.go
+++ json_test.go
@@ -1,10 +1,11 @@
 package guti
 
 import (
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestJSONToMap(t *testing.T) {
