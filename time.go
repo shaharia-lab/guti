0ea7f110211acb695c262f38aaca0f3bd6e41bbd
Shaharia Azam <mail@shaharia.com>
2023-04-09 12:29:00 +0200

    RetryWithExponentialBackoff function added

--- time.go
+++ time.go
@@ -1,7 +1,10 @@
 // Package guti contains packages
 package guti
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // Retry allows you to retry a given function a certain number of times until it succeeds or the retries are exhausted
 func Retry(f func() error, retries int, sleep time.Duration) error {
@@ -14,4 +17,27 @@ func Retry(f func() error, retries int, sleep time.Duration) error {
 		time.Sleep(sleep)
 	}
 	return err
+}
+
+// RetryWithExponentialBackoff retries a function with exponential backoff in case of errors
+func RetryWithExponentialBackoff(fn func() error, maxRetries int, initialBackoffSeconds int) error {
+	backoff := time.Duration(initialBackoffSeconds) * time.Second
+	subsequentBackoff := 2
+
+	for i := 0; ; i++ {
+		fmt.Println("Retrying....")
+		err := fn()
+		if err == nil {
+			return nil
+		}
+
+		if i == maxRetries {
+			return fmt.Errorf("maximum number of retries exceeded: %w", err)
+		}
+
+		//log.Printf("Error occurred: %v. Retrying in %v", err, backoff)
+		time.Sleep(backoff)
+
+		backoff *= time.Duration(subsequentBackoff)
+	}
 }
--- time_test.go
+++ time_test.go
@@ -2,6 +2,7 @@ package guti
 
 import (
 	"errors"
+	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
 )
@@ -21,5 +22,50 @@ func TestRetry(t *testing.T) {
 
 	if err != nil {
 		t.Errorf("Unexpected error: %v", err)
+	}
+}
+
+func TestRetryWithExponentialBackoff(t *testing.T) {
+	testCases := []struct {
+		name        string
+		callback    func() error
+		expectError bool
+	}{
+		{
+			name: "function returns error",
+			callback: func() error {
+				return errors.New("error")
+			},
+			expectError: true,
+		},
+		{
+			name: "function returns no error",
+			callback: func() error {
+				return nil
+			},
+			expectError: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Define the maximum number of retries and the backoff intervals
+			maxRetries := 1
+			initialBackoff := 1
+
+			// Call the function with retry and check the result
+			err := RetryWithExponentialBackoff(tc.callback, maxRetries, initialBackoff)
+
+			if tc.expectError {
+				assert.Error(t, err)
+				// Check the error message
+				expectedErrorMessage := "maximum number of retries exceeded: error"
+				if err.Error() != expectedErrorMessage {
+					t.Errorf("unexpected error message: got %v, want %v", err.Error(), expectedErrorMessage)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+		})
 	}
 }
