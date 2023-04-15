cf9f4de8ca25a32b6da88d1c195d52f2a7a1d5a7
Shaharia Azam <mail@shaharia.com>
2023-04-15 23:24:13 +0200

    Added more comments for few methods

--- guti.go
+++ guti.go
@@ -4,7 +4,21 @@ package guti
 import "reflect"
 
 // GetTypeName returns the name of the type of the given object.
-// If the object is a pointer, it returns the name of the pointed-to type.
+// If the object is a pointer, it returns the name of the pointed-to type with a
+// leading asterisk (*).
+//
+// The method uses reflection to determine the type of the input object, and then
+// returns the name of the type as a string. If the input object is a pointer, it
+// returns the name of the pointed-to type with a leading asterisk (*).
+//
+// This method can be useful in situations where you need to get the name of the
+// type of an object dynamically, such as in debugging, logging, or error handling.
+//
+// Example usage:
+//   var myInt int
+//   var myPtr *MyStruct
+//   fmt.Println(GetTypeName(myInt))   // prints "int"
+//   fmt.Println(GetTypeName(myPtr))   // prints "*MyStruct"
 func GetTypeName(myvar interface{}) string {
 	t := reflect.TypeOf(myvar)
 
